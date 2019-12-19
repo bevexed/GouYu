61cf879c3d03bceffe4842b966043cd337fe4aff
bevexed <547551650@qq.com>
2019-12-19 11:14:09 +0800

    健康大人SHOW

--- src/containers/health-page/health-index/star-show-banner/index.tsx
+++ src/containers/health-page/health-index/star-show-banner/index.tsx
@@ -1,33 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './index.less';
-import { Carousel, WingBlank } from 'antd-mobile';
+import { WingBlank } from 'antd-mobile';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.min.css';
 
 type Props = {
   bannerList: any[];
 };
 const StarShowBanner: FC<Props> = (props: Props) => {
+  useEffect(() => {
+    new Swiper('.swiper-container', {
+      direction: 'horizontal', // 垂直切换选项
+      freeMode: true,
+      slidesPerView: 2.3,
+    });
+  });
   return (
     <WingBlank className={ 'star-show-banner' }>
-      <Carousel
-        className="space-carousel"
-        frameOverflow="hidden"
-        dots={ false }
-        cellSpacing={ 12 }
-        slideWidth='141px'
-        beforeChange={ (from, to) => console.log(`slide from ${ from } to ${ to }`) }>
-        { props.bannerList.map((val, index) => (
-          <img
-            key={ index }
-            src={ `https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=334699929,3250564392&fm=26&gp=0.jpg` }
-            alt=""
-            className={ 'img' }
-            onLoad={ () => {
-              // fire window resize event to change height
-              window.dispatchEvent(new Event('resize'));
-            } }
-          />
-        )) }
-      </Carousel>
+      <div className="swiper-container">
+        <div className="swiper-wrapper">
+          { props.bannerList.map((val, index) => (
+            <div className="swiper-slide" key={ index }>
+              <img
+                src={ `https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=334699929,3250564392&fm=26&gp=0.jpg` }
+                alt=""
+                className={ 'img' }
+                onLoad={ () => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event('resize'));
+                } }
+              />
+            </div>
+          )) }
+        </div>
+      </div>
     </WingBlank>
   );
 };
